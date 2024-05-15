d2ce4e78ee977011ccb0607cb7a7482bebe7bec6
Azharafeed <125554523+Azharafeed@users.noreply.github.com>
2024-05-15 17:25:51 +0530

    here i showed posts posted by the previes users

--- controlers/home_controler.js
+++ controlers/home_controler.js
@@ -1,3 +1,11 @@
-module.exports.home = function(req,res){
-    return res.render('index')
-}
+const Post = require('../model/post');
+
+module.exports.home = async function(req, res) {
+    try {
+        const posts = await Post.find({}).exec();
+        return res.render('index', { posts: posts });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'An error occurred while fetching posts' });
+    }
+};
