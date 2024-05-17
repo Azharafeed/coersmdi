92561aea36e76dc9e3634db327c0f41c58422d13
Azharafeed <125554523+Azharafeed@users.noreply.github.com>
2024-05-17 19:11:55 +0530

    added authoriation to post ,only a logedin user can post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+const passport = require('passport')
 
 const postControler = require('../controlers/posts_controler');
 
-router.post('/create',postControler.create);
+router.post('/create',passport.checkAuthentication,postControler.create);
 
 module.exports = router;
